ce60323a11d7459ef2f08b16dd5ee1a2762bbaff
aballerina22 <32208413+aballerina22@users.noreply.github.com>
2018-03-22 12:07:32 -0700

    Add files via upload

--- SrProject/predictData.m
+++ SrProject/predictData.m
@@ -1,25 +1,37 @@
 
 function [data] = predictData(var)
-        load('walkRun.mat')
-        load('walkJump.mat')
-        load('walkFall.mat')
-        load('standRun.mat')
-        load('standJump.mat')
-        load('standFall.mat')
-        load('waveRun.mat')
-        load('waveJump.mat')
-        load('waveFall.mat')   
-        data = [];
-        
-for i = 1:height(var)
-        modelType = standJump.ClassificationEnsemble;
-        [predictedStandA,scoreStandA] = predict(modelType, var(i,:));
-        modelType = standFall.ClassificationEnsemble;
-        [predictedStandB,scoreStandB] = predict(modelType, var(i,:));
-        modelType = standRun.ClassificationEnsemble;
-        [predictedStandC,scoreStandC] = predict(modelType, var(i,:));
-        predictedStand = [predictedStandA(end),predictedStandB(end),predictedStandC(end)];
-        DataLine = [var(i,:), predictedStand]
+    load('walkRun.mat')
+    load('walkJump.mat')
+    load('walkFall.mat')
+    load('standRun.mat')
+    load('standJump.mat')
+    load('standFall.mat')
+    load('waveRun.mat')
+    load('waveJump.mat')
+    load('waveFall.mat')
+    data = [];
+
+    % for i = 1:height(var)
+    %         modelType = standJump.ClassificationEnsemble;
+    %         [predictedStandA,scoreStandA] = predict(modelType, var(i,1:60));
+    %         modelType = standFall.ClassificationEnsemble;
+    %         [predictedStandB,scoreStandB] = predict(modelType, var(i,1:60));
+    %         modelType = standRun.ClassificationEnsemble;
+    %         [predictedStandC,scoreStandC] = predict(modelType, var(i,1:60));
+    %         predictedStand = [predictedStandA(end),predictedStandB(end),predictedStandC(end)];
+    %         DataLine = [var(i,:), predictedStand];
+    %         data = [data; DataLine];
+    % end
+    for i = 1:height(var)
+        modelType = walkJump.ClassificationEnsemble;
+        [predictedWalkA,scoreWalkA] = predict(modelType, var(i,1:60));
+        modelType = walkFall.ClassificationEnsemble;
+        [predictedWalkB,scoreWalkB] = predict(modelType, var(i,1:60));
+        modelType = walkRun.ClassificationEnsemble;
+        [predictedWalkC,scoreWalkC] = predict(modelType, var(i,1:60));
+        predictedWalk = [predictedWalkA(end),predictedWalkB(end),predictedWalkC(end)];
+        DataLine = [var(i,:), predictedWalk];
         data = [data; DataLine];
     end
+
 end
