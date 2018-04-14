87f9cca3dbcb89695f33ce76196193ad79ae9fb2
aballerina22 <32208413+aballerina22@users.noreply.github.com>
2018-04-14 13:00:55 -0700

    Add files via upload

--- SrProject/runAbnormML2.m
+++ SrProject/runAbnormML2.m
@@ -6,26 +6,31 @@ load('walkFall.mat')
 load('standRun.mat')
 load('standJump.mat')
 load('standFall.mat')
-load('waveRun.mat')
-load('waveJump.mat')
-load('waveFall.mat')
+load('WaveRun2.mat')
+load('WaveJump2.mat')
+load('WaveFall2.mat')
+load('newWalkRun.mat')
 %load('st23SVM.mat')
 load('st23predictnoConfidence.mat')
-
-load('hmm_data_matrix.mat')
-any2W = s;
-load('hmm_data_matrix_anyWalk.mat')
-anyW = s;
+load('WaveStand2.mat')
+load('WaveWalk2.mat')
+% load('hmm_data_matrix.mat')
+% any2W = s;
+ load('new_hmm_training_data.mat')
+ load('new_hmm_training_data_2state.mat')
+% anyW = s;
+%load('hmm_data_matrix_anyWalk.mat')
+stateList = [];
+stateList2 = [];
 TRGUESS = [[.85],[.15];[.4],[.6]];
-EMITGUESS = [[.4],[.45],[.05];[.05],[.05],[.9]];
-[ESTTR_2W,ESTEMIT_2W] = hmmtrain(any2W,TRGUESS,EMITGUESS);
-[ESTTR_anyW,ESTEMIT_anyW] = hmmtrain(anyW,TRGUESS,EMITGUESS);
-
+EMITGUESS2 = [[.4],[.45],[.05];[.05],[.05],[.9]];
+EMITGUESS = [[.33],[.30],[.32],[.05];[.03],[.04],[.03],[.9]];
+% % [ESTTR_2W,ESTEMIT_2W] = hmmtrain(any2W,TRGUESS,EMITGUESS);
+% % [ESTTR_anyW,ESTEMIT_anyW] = hmmtrain(anyW,TRGUESS,EMITGUESS);
+[ESTTR,ESTEMIT] = hmmtrain(new_hmm_training_data,TRGUESS,EMITGUESS);
+[ESTTR2,ESTEMIT2] = hmmtrain(new_hmm_training_data_2state,TRGUESS,EMITGUESS2);
 % Initialize Camera
 [vid, depthVid, himg, src] = InitializeKinect();
-% global firstLoop = 1;
-% global oldData = 0;
-% global oldDataJWC = 0;
 firstLoop = 1;
 oldData = 0;
 oldDataJWC = 0;
@@ -44,10 +49,9 @@ while ishandle(himg)
     trigger(depthVid);
     [depthMap, ~, depthMetaData] = getdata(depthVid);
     imshow(depthMap, [0 4096]);
-
     
     if sum(depthMetaData.IsSkeletonTracked) > 0
-        loop = loop+1;
+        
         row = row +1;
         d = depthMetaData;
         % Prep to log data in one line
@@ -61,16 +65,17 @@ while ishandle(himg)
        skeletonJoints = depthMetaData.JointDepthIndices(:,:,depthMetaData.IsSkeletonTracked);
        allPlaces = [];
        if (firstLoop ==0)
+           loop = loop+1;
            for i = 1:numberOfPeople
 %                print('i :')
-               display(i)
+               %display(i)
                thisJWC = VelocityDiffJWC(1,(person(i)-1)*60+1:person(i)*60);
                modelType = walkJump.ClassificationEnsemble; 
                [predictedWalkA,scoreWalkA] = predict(modelType, thisJWC);
-                display('Got here')
+              
                modelType = walkFall.ClassificationEnsemble; 
                [predictedWalkB,scoreWalkB] = predict(modelType, thisJWC);
-               modelType = walkRun.ClassificationEnsemble; 
+               modelType = newWalkRun.ClassificationEnsemble; 
                [predictedWalkC,scoreWalkC] = predict(modelType, thisJWC);
                predictedWalk = [predictedWalkA(end),predictedWalkB(end),predictedWalkC(end)];
            %    display(predictedWalk);
@@ -87,65 +92,119 @@ while ishandle(himg)
                scoreStand = [scoreStand;[scoreStandA(end),scoreStandB(end),scoreStandC(end)]];
                
                                     
-               modelType = waveJump.ClassificationEnsemble; 
-                [predictedWaveA,scoreWaveA] = predict(modelType, thisJWC);
-               modelType = waveFall.ClassificationEnsemble; 
-               [predictedWaveB,scoreWaveB] = predict(modelType, thisJWC);
-               modelType = waveRun.ClassificationKNN; 
-               [predictedWaveC,scoreWaveC] = predict(modelType, thisJWC);
-               predictedWave = [predictedWaveA(end),predictedWaveB(end),predictedWaveC(end)];
+               modelType = WaveJump2.ClassificationSVM; 
+                [predictedWaveA,scoreWaveA] = predict(modelType, thisJWC(13:36));
+               modelType = WaveFall2.ClassificationEnsemble; 
+               [predictedWaveB,scoreWaveB] = predict(modelType, thisJWC(13:36));
+               modelType = WaveRun2.ClassificationEnsemble; 
+               [predictedWaveC,scoreWaveC] = predict(modelType, thisJWC(13:36));
+               
+                modelType = WaveStand2.ClassificationEnsemble; 
+               [predictedWaveD,scoreWaveD] = predict(modelType, thisJWC(13:36));
+               modelType = WaveWalk2.ClassificationEnsemble; 
+               [predictedWaveE,scoreWaveE] = predict(modelType, thisJWC(13:36));
+               
+               predictedWave = [predictedWaveA(end),predictedWaveB(end),predictedWaveC(end),predictedWaveD(end),predictedWaveE(end)];
                scoreWave = [scoreWave;[scoreWaveA,scoreWaveB,scoreWaveC]];
             %   display(predictedWave);
                
-               
-               if(((strcmp(predictedStandA,'Stand')&&strcmp(predictedStandB,'Stand'))...
+%                
+               if ((strcmp(predictedStandA,'Stand')&&strcmp(predictedStandB,'Stand'))...
                        || (strcmp(predictedStandA,'Stand')&&strcmp(predictedStandC,'Stand'))...
-                       || (strcmp(predictedStandB,'Stand')&&strcmp(predictedStandC,'Stand')))...
-                       ||((strcmp(predictedWaveA,'Wave')&&strcmp(predictedWaveB,'Wave'))...
+                       || (strcmp(predictedStandB,'Stand')&&strcmp(predictedStandC,'Stand')))
+                        Move = 'Stand';
+                        stateKey = 3;
+                        stateKey2 = 1;
+
+
+               elseif((strcmp(predictedWaveA,'Wave')&&strcmp(predictedWaveB,'Wave'))...
                        || (strcmp(predictedWaveA,'Wave')&&strcmp(predictedWaveC,'Wave'))...
-                       || (strcmp(predictedWaveB,'Wave')&&strcmp(predictedWaveC,'Wave'))))
-                   state = 'Stand/Wave';
+                       || (strcmp(predictedWaveA,'Wave')&&strcmp(predictedWaveD,'Wave'))...
+                       || (strcmp(predictedWaveA,'Wave')&&strcmp(predictedWaveE,'Wave'))...
+                       || (strcmp(predictedWaveB,'Wave')&&strcmp(predictedWaveC,'Wave'))...
+                       || (strcmp(predictedWaveB,'Wave')&&strcmp(predictedWaveD,'Wave'))...
+                       || (strcmp(predictedWaveB,'Wave')&&strcmp(predictedWaveE,'Wave'))...
+                       || (strcmp(predictedWaveC,'Wave')&&strcmp(predictedWaveD,'Wave'))...
+                       || (strcmp(predictedWaveC,'Wave')&&strcmp(predictedWaveE,'Wave'))...
+                       || (strcmp(predictedWaveD,'Wave')&&strcmp(predictedWaveE,'Wave')))
+                   Move = 'Wave';
                    stateKey = 1;
-               elseif (strcmp(predictedWalkA,'Walk')...
-                       ||strcmp(predictedWalkB,'Walk')...
-                       ||strcmp(predictedWalkC,'Walk'))
-                   state = 'Walk';
-                   stateKey = 2;
-               else 
-                   state = 'Abnormal';
-                   stateKey = 3;
+                   stateKey2 = 1;
+%                else
+%                    Wave = 'Abnormal';
+               elseif ((strcmp(predictedWalkA,'Walk')&&strcmp(predictedWalkB,'Walk'))...
+                       || (strcmp(predictedWalkA,'Walk')&&strcmp(predictedWalkC,'Walk'))...
+                       || (strcmp(predictedWalkB,'Walk')&&strcmp(predictedWalkC,'Walk')))
+                        Move = 'Walk';
+                        stateKey = 2;
+                        stateKey2 = 2;
+%                elseif ((strcmp(predictedStandA,'Stand')&&strcmp(predictedStandB,'Stand'))...
+%                        || (strcmp(predictedStandA,'Stand')&&strcmp(predictedStandC,'Stand'))...
+%                        || (strcmp(predictedStandB,'Stand')&&strcmp(predictedStandC,'Stand')))
+%                         Move = 'Stand';
+%                         stateKey = 3;
+                else
+                        Move = 'Abnormal';
+                        stateKey = 4;
+                        stateKey2 = 3;
                end
-               
-               fid = fopen('test.csv', 'w') ;
-               filename = sprintf('JWC_hmm_labels_anyWalk.csv');
-               dataLine = [thisJWC,stateKey];
-               dlmwrite(filename,dataLine,'-append','delimiter',',')
-               fclose(fid)
+                
+               stateList = [stateList;stateKey];
+               stateList2 = [stateList2;stateKey2];
+%                        ||strcmp(predictedWalkB,'Walk')...
+%                        ||strcmp(predictedWalkC,'Walk'))
+%                    state = 'Walk';
+%                    stateKey = 2;
+%                else 
+%                    state = 'Abnormal';
+%                    stateKey = 3;
+%                end
+%                
                if (loop > 4)
-                   last3 = [last3,stateKey];
-                   last3 = last3(1,2:4);
-                   last3Name = [last3Name,state];
-                   last3Name = last3Name(1,2:4);
-                   STATES = hmmviterbi(last3,ESTTR_2W,ESTEMIT_2W);
-                   STATES_anyW = hmmviterbi(last3,ESTTR_anyW,ESTEMIT_anyW);
+%                    last3 = [last3,stateKey];
+%                    last3 = last3(1,2:4);
+%                    last3Name = [last3Name,state];
+%                    last3Name = last3Name(1,2:4);
+                   STATES = hmmviterbi(transpose(stateList(end-4:end,1)),ESTTR,ESTEMIT);
+                   STATES2 = hmmviterbi(transpose(stateList2(end-4:end,1)),ESTTR2,ESTEMIT2);
+                   %STATES_anyW = hmmviterbi(last3,ESTTR_anyW,ESTEMIT_anyW);
                elseif (loop <= 4)
-                   last3 = [last3,stateKey];
-                   last3Name = [last3Name,state];
-                   STATES = hmmviterbi(last3,ESTTR_2W,ESTEMIT_2W);
-                   STATES_anyW = hmmviterbi(last3,ESTTR_anyW,ESTEMIT_anyW);
+%                    last3 = [last3,stateKey];
+%                    last3Name = [last3Name,state];
+%                    STATES = hmmviterbi(last3,ESTTR_2W,ESTEMIT_2W);
+%                    STATES_anyW = hmmviterbi(last3,ESTTR_anyW,ESTEMIT_anyW);
+                    STATES = hmmviterbi(transpose(stateList(end,1)),ESTTR,ESTEMIT);
+                    STATES2 = hmmviterbi(transpose(stateList2(end,1)),ESTTR2,ESTEMIT2);
                end
-               
-               if (ismember(2,STATES_anyW))
-                   if (ismember(2,STATES))
-                       report = 'Abnormal'
-                   end
-               else
-                   report = 'Normal'
-               end
-               display (report)
-               
-               allPlaces = {char(last3Name),char(num2str(STATES)),char(num2str(STATES_anyW)),char(report)};
-               scoreTable = [scoreTable; [scoreWalk(end),scoreStand(end),scoreWave(end)]];
+%                
+%                if (ismember(2,STATES_anyW))
+%                    if (ismember(2,STATES))
+%                        report = 'Abnormal'
+%                        reportnum = 2;
+% %                        send_text_message('925-337-5087','Verizon', 'Smart Camera: Security Alert!')
+% %                        sendmail('aweber13@lion.lmu.edu','Smart Security System: Alert!','Smart Security System: Alert!')
+%                    end
+%                else
+%                    report = 'Normal'
+%                    reportnum = 1;
+%                end
+%                display (report)
+%                
+%                fid = fopen('test.csv', 'w') ;
+% %                filename = sprintf('JWC_hmm_labels_Walk_Mohammed.csv');
+%                 filename = sprintf('JWC_Left_Wave_Amy.csv');
+%                dataLine = [thisJWC];
+%                dlmwrite(filename,dataLine,'-append','delimiter',',')
+%                fclose(fid)
+%                
+%                allPlaces = {char(last3Name),char(num2str(STATES)),char(num2str(STATES_anyW)),char(report)};
+%                scoreTable = [scoreTable; [scoreWalk(end),scoreStand(end),scoreWave(end)]];
+                 if (length(stateList)>5)
+                       display(transpose(stateList2(end-4:end,1)));
+                 else
+                     display(stateList2(end,1));
+                 end
+                 %scoreTable = [scoreTable; [scoreWalk(end),scoreStand(end),scoreWave(end)]];
            end
            allLabels = [];
            lineOptions = [{':o'}, {':go'},{':ko'}, {':ro'}, {':po'}, {':yo'}];
@@ -155,8 +214,10 @@ while ishandle(himg)
                plot(skeletonJoints(:,1,i),skeletonJoints(:,2,i),currentSym);
            end
            hold off;
-           allPlaces = char(allPlaces);
-           lgd = legend(allPlaces);
+           display('STATES: ')
+           display(STATES2)
+%            allPlaces = {char(STATES)};
+%            lgd = legend(allPlaces);
            lgd.FontSize = 20;
 %            set(gcf,'units','normalized','outerposition',[0 0 1 1])
        end
